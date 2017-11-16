aa8e75f73de6883bc9e0a20b0a51b0696b956939
Daniel Martins <xpdaniel2011@gmail.com>
2017-11-16 14:11:47 -0200

    Method constructor adicionado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { 
+      description: '', 
+      todos: [
+        { description: 'Comprar o presente do meu irmão hoje!', done: true },
+        { description: 'Ir ao supermercado fazer as compras.', done: false },
+        { description: 'Assistir palestra sobre ReactJS amanhã às 11h', done: false }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,21 +26,13 @@ class App extends Component {
         </div>
         <div>
           <table>
-            <tr>
-              <td>Comprar o presente do meu irmão hoje!</td>
-              <td><button className="btn-todo">&#10003;</button></td>
-              <td><button className="btn-todo">&#10799;</button></td>
-            </tr>
-            <tr>
-              <td>Ir ao supermercado fazer as compras.</td>
-              <td><button className="btn-todo">&#10003;</button></td>
-              <td><button className="btn-todo">&#10799;</button></td>
-            </tr>
-            <tr>
-              <td>Assistir palestra sobre ReactJS amanhã às 11h</td>
-              <td><button className="btn-todo">&#10003;</button></td>
-              <td><button className="btn-todo">&#10799;</button></td>
-            </tr>
+            {this.state.todos.map((todo, index) => (
+              <tr>
+                <td>{todo.description}</td>
+                {!todo.done && <td><button className="btn-todo">&#10003;</button></td>}
+                {todo.done && <td><button className="btn-todo">&#10799;</button></td>}
+              </tr>
+            ))}
           </table>
         </div>
       </div>
