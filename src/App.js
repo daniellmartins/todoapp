db7b918ba6470aeba23d5975960e22208eef13cc
Daniel Martins <xpdaniel2011@gmail.com>
2017-11-16 13:37:34 -0200

    Estrutura do App.js pronta

--- src/App.js
+++ src/App.js
@@ -5,6 +5,30 @@ class App extends Component {
     return (
       <div className="App">
         <h1>TodoApp</h1>
+        <hr/>
+        <div>
+          <input type="text" placeholder="Ex: Comprar presente amanhã as 18h"/>
+          <button className="btn-add-todo">Adicionar</button>
+        </div>
+        <div>
+          <table>
+            <tr>
+              <td>Comprar o presente do meu irmão hoje!</td>
+              <td><button className="btn-todo">&#10003;</button></td>
+              <td><button className="btn-todo">&#10799;</button></td>
+            </tr>
+            <tr>
+              <td>Ir ao supermercado fazer as compras.</td>
+              <td><button className="btn-todo">&#10003;</button></td>
+              <td><button className="btn-todo">&#10799;</button></td>
+            </tr>
+            <tr>
+              <td>Assistir palestra sobre ReactJS amanhã às 11h</td>
+              <td><button className="btn-todo">&#10003;</button></td>
+              <td><button className="btn-todo">&#10799;</button></td>
+            </tr>
+          </table>
+        </div>
       </div>
     )
   }
